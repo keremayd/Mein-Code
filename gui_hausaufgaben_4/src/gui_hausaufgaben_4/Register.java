523991b75d96e676a4637f0671a12f1ae71cbc9c
Kerem Aydın <kerem.aydin@turk.net>
2023-05-19 01:14:43 +0300

    Register v0.2

--- gui_hausaufgaben_4/src/gui_hausaufgaben_4/Register.java
+++ gui_hausaufgaben_4/src/gui_hausaufgaben_4/Register.java
@@ -2,6 +2,14 @@ package gui_hausaufgaben_4;
 
 import java.awt.Color;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -9,8 +17,8 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class Register extends JFrame {
-	
+public class Register extends JFrame implements ActionListener{
+	private String path = "C:\\Users\\kerem.aydın\\git\\repository\\gui_hausaufgaben_4\\src\\gui_hausaufgaben_4\\users";
 	private JButton btnRegister;
 	private JLabel lblId, lblPassword;
 	private JTextField id, password;
@@ -25,6 +33,7 @@ public class Register extends JFrame {
 		lblPassword = new JLabel("Password");
 		password = new JTextField();
 		btnRegister = new JButton("Register");
+		btnRegister.addActionListener(this);
 		
 		lblId.setForeground(Color.BLACK);
 		lblPassword.setForeground(Color.BLACK);
@@ -53,6 +62,31 @@ public class Register extends JFrame {
 	public static void main(String[] args) {
 		new Register();
 
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(id.getText().equals("") != true && password.getText().equals("") != true) {
+			File file = new File(path);
+			try {
+				FileReader fReader = new FileReader(file);
+				BufferedReader bReader = new BufferedReader(fReader);
+				String line = "";
+				while((line = bReader.readLine()) != null) { //file komple boşsa hiç çalışmıyor buna çözüm lazım
+					String[] user = line.split("\t");
+					if(id.getText().equals(user[0]) == false) {
+						FileWriter fWriter = new FileWriter(file,true);
+						BufferedWriter bWriter = new BufferedWriter(fWriter);
+						bWriter.write(id.getText()+"\t"+password.getText()+"\t"+0+"\n");
+						bWriter.close();
+						break;
+					}
+				}
+				bReader.close();
+			} catch (IOException e1) {
+				System.out.println(e1.getMessage());
+			}
+		}
 	}
 
 }
